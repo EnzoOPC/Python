4cac8abb4494acf41f2b9c17e319a403641bcfed
Enzo de Oliveira Passos Correia <Enzo.OPC1@gmail.com>
2024-10-06 02:01:10 -0300

    Start Over

---
+++ demo/myApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+from .models import TodoITem
+
+# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+def todos(request):
+    items = TodoITem.objects.all()
+    return render(request, "todos.html", {"todos": items})
--- storefront/playground/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-# URL COnf module
-urlpatterns = [
-    path("hello/", views.say_hello)
-]
